6c10edc72dc3ad8b1f40a87675b52532e73103c8
Dmitry <once87@yandex.ru>
2023-05-30 18:18:34 +0300

    Test

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnData() {
+
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Здравствуй, мир")
+
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Здравствуй, мир"))
+                .contentType(ContentType.JSON)
+
+        ;
+    }
+}
